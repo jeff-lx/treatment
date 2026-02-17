ad4b1b1bc3ec888fa953b0a6df7d360233733a85
Jeff Fisher <jeff.fisher@levelex.com>
2026-02-16 20:12:36 -0500

    fix: adjust relative base path

--- app/vite.config.js
+++ app/vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // makes all asset paths relative
   plugins: [react(), tailwindcss()],
 })
 
